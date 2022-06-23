3da44863e12dbd785b71b66c03d6814c241e41ae
Armored <108043050+Armored-22@users.noreply.github.com>
2022-06-22 22:26:44 -0700

    Update Grave-Rejects.bat

--- Grave-Rejects.bat
+++ Grave-Rejects.bat
@@ -1,1 +1,48 @@
+@echo off
+set SteamAppId=1604030
 
+REM Layout = W:75 / H:40
+
+echo.
+echo 	            Server Launcher For Grave Rejects
+echo.
+
+:::                                .       .
+:::                 _.-'\          |\-"""-/|          /`-._
+:::             _.-`     `.       /         \       ,'     '-._
+:::          _.'           `._   ;   \   /   ;   _,'           `._
+:::        .'                 `-.:           :.-'                 `.
+:::      ,`                           , ,                           '.
+:::    ,`                                                             '.
+:::   /                                                                 \
+:::  :,-"""-,                    Grave Rejects                    ,-"""-,:
+::: /'       `                                                   '       '\
+:::          :                                                   :
+:::          : ,-"""-,                                   ,-"""-, :
+:::          /'       `.       _.-'         '-._       .'       '\
+:::                     \    .`    :       :    '.    /
+:::                      . .`       :     :       '. .
+:::                      :/          :   :          \:
+:::                      :            : :            :
+:::                                    :
+:::
+
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+REM Place ":::" at the start of each line for ASCII characters.
+
+echo.
+echo         Gettings Ready To Start The V Rising Dedicated Server
+echo                    PRESS CTRL+C To Shutdown Server          
+echo.
+
+pause
+
+echo.
+echo Server Launched;
+echo %DATE%
+Time /T
+echo.
+
+pause
+
+@echo on
